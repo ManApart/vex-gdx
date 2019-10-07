56ef07aeb23dd44ff168b9ff100458517d4423ed
ManApart <raustinkucera@gmail.com>
2019-10-07 12:26:51 -0400

    wall jump and dashes

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -4,10 +4,13 @@ import kotlin.math.abs
 
 const val GRAVITY = 20.0f
 private const val ACCELERATION = 20f
+
 private const val JUMP_VELOCITY = 10f
+private const val WALL_JUMP_KICKOFF_VELOCITY = 6f
 private const val JUMP_TIME = .1f
-private const val DASH_VELOCITY = 50f
-private const val DASH_TIME = 50f
+
+private const val DASH_VELOCITY = 20f
+private const val DASH_TIME = .15f
 
 
 class Player(map: Map) : RigidBodyOwner {
@@ -22,16 +25,32 @@ class Player(map: Map) : RigidBodyOwner {
 
     override fun onCollided(direction: Direction) {
 //        println("Collided $direction")
-        if (direction == Direction.DOWN) {
-            setPlayerState(PlayerState.RUNING)
-            hasDoubleJump = true
+        when (direction) {
+            Direction.UP -> {
+            }
+            Direction.DOWN -> {
+                if (state != PlayerState.DASHING) {
+                    setPlayerState(PlayerState.RUNING)
+                    hasDoubleJump = true
+                }
+            }
+            Direction.LEFT -> {
+                if (state == PlayerState.DASHING) {
+                    setStoppedState()
+                }
+            }
+            Direction.RIGHT -> {
+                if (state == PlayerState.DASHING) {
+                    setStoppedState()
+                }
+            }
         }
     }
 
     override fun onNoLongerCollided(direction: Direction) {
 //        println("No longer Collided $direction")
-        if (direction == Direction.DOWN){
-            if (state != PlayerState.JUMPING){
+        if (direction == Direction.DOWN) {
+            if (!state.isInState(PlayerState.JUMPING, PlayerState.DASHING)) {
                 setPlayerState(PlayerState.FALLING)
             }
         }
@@ -46,49 +65,56 @@ class Player(map: Map) : RigidBodyOwner {
     fun update(deltaTime: Float) {
         processKeys()
         updateState(deltaTime)
-        body.update(deltaTime)
+        body.update(deltaTime, ignoreXMax(), ignoreYMax())
 
 //        println("Player is: ${body.bounds.x}, ${body.bounds.y}")
     }
 
     private fun processKeys() {
         if (Controller.jump.isFirstPressed()) {
-            if (body.isCollided(Direction.DOWN)) {
+            if (state != PlayerState.JUMPING && body.isCollidedAny(Direction.DOWN, Direction.LEFT, Direction.RIGHT)) {
                 setPlayerState(PlayerState.JUMPING)
+                if (!isGrounded()) {
+                    if (body.isCollided(Direction.RIGHT)) {
+                        body.vel.x = -WALL_JUMP_KICKOFF_VELOCITY
+                    } else if (body.isCollided(Direction.LEFT)) {
+                        body.vel.x = WALL_JUMP_KICKOFF_VELOCITY
+                    }
+                }
             } else if (hasDoubleJump) {
                 hasDoubleJump = false
                 setPlayerState(PlayerState.JUMPING)
             }
         }
 
-//        if (Controller.dashLeft.isFirstPressed()) {
-//            if (state != PlayerState.DASHING) {
-//                dir = Direction.LEFT
-//                setPlayerState(PlayerState.DASHING)
-//            }
-//        }
-//
-//        if (Controller.dashRight.isFirstPressed()) {
-//            if (state != PlayerState.DASHING) {
-//                dir = Direction.RIGHT
-//                setPlayerState(PlayerState.DASHING)
-//            }
-//        }
+        if (Controller.dashLeft.isFirstPressed()) {
+            if (state != PlayerState.DASHING) {
+                dir = Direction.LEFT
+                setPlayerState(PlayerState.DASHING)
+            }
+        }
+
+        if (Controller.dashRight.isFirstPressed()) {
+            if (state != PlayerState.DASHING) {
+                dir = Direction.RIGHT
+                setPlayerState(PlayerState.DASHING)
+            }
+        }
 
         if (abs(Controller.xAxis.value) > 0) {
-            if (body.isCollided(Direction.DOWN) && state == PlayerState.IDLE) {
+            if (isGrounded() && state == PlayerState.IDLE) {
                 setPlayerState(PlayerState.RUNING)
             }
             dir = Direction.fromNumber(Controller.xAxis.value)
             body.accel.x = ACCELERATION * Controller.xAxis.value
         } else if (state == PlayerState.RUNING) {
-            if (body.isCollided(Direction.DOWN)) {
+            if (isGrounded()) {
                 setPlayerState(PlayerState.IDLE)
             }
         }
 
         if (abs(Controller.yAxis.value) > 0) {
-            body.accel.y = ACCELERATION * Controller.yAxis.value
+//            body.accel.y = ACCELERATION * Controller.yAxis.value
         }
     }
 
@@ -108,7 +134,7 @@ class Player(map: Map) : RigidBodyOwner {
                 body.vel.y = 0f
                 body.vel.x = dir.vector.x * DASH_VELOCITY
             } else {
-                setPlayerState(PlayerState.IDLE)
+                setStoppedState()
             }
         }
 
@@ -121,8 +147,27 @@ class Player(map: Map) : RigidBodyOwner {
         }
     }
 
+    private fun isGrounded() = body.isCollided(Direction.DOWN)
+
+    private fun ignoreXMax(): Boolean {
+        return state.isInState(PlayerState.DASHING)
+    }
+
+    private fun ignoreYMax(): Boolean {
+        return state.isInState(PlayerState.JUMPING)
+    }
+
+    private fun setStoppedState() {
+        if (isGrounded()) {
+            setPlayerState(PlayerState.IDLE)
+        } else {
+            setPlayerState(PlayerState.FALLING)
+        }
+    }
+
     fun draw(batch: SpriteBatch) {
         drawable.draw(batch, body.bounds, state, dir, stateTime)
     }
+
 
 }
--- src/main/kotlin/RigidBody.kt
+++ src/main/kotlin/RigidBody.kt
@@ -2,7 +2,6 @@ import com.badlogic.gdx.math.MathUtils.clamp
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 
-//const val GRAVITY = 20.0f
 const val MAX_X_VEL = 6f
 const val MAX_Y_VEL = 10f
 const val DAMP = 0.90f
@@ -17,6 +16,10 @@ class RigidBody(private val map: Map, private val owner: RigidBodyOwner, width: 
         val map = mutableMapOf<Direction, Boolean>()
         Direction.values().forEach { map[it] = false }
         return map
+    }
+
+    fun isCollidedAny(vararg directions: Direction): Boolean {
+        return directions.any { isCollided(it) }
     }
 
     fun isCollided(direction: Direction): Boolean {
@@ -39,14 +42,18 @@ class RigidBody(private val map: Map, private val owner: RigidBodyOwner, width: 
         }
     }
 
-    fun update(deltaTime: Float) {
-//        accel.y = -GRAVITY
+    fun update(deltaTime: Float, ignoreXMax: Boolean = false, ignoreYMax: Boolean = false) {
         accel.scl(deltaTime)
         vel.add(accel.x, accel.y)
         if (accel.x == 0f) vel.x *= DAMP
 
-        vel.x = clamp(vel.x, -MAX_X_VEL, MAX_X_VEL)
-        vel.y = clamp(vel.y, -MAX_Y_VEL, MAX_Y_VEL)
+        if (!ignoreXMax) {
+            vel.x = clamp(vel.x, -MAX_X_VEL, MAX_X_VEL)
+        }
+
+        if (!ignoreYMax) {
+            vel.y = clamp(vel.y, -MAX_Y_VEL, MAX_Y_VEL)
+        }
 
         vel.scl(deltaTime)
         tryMove()
